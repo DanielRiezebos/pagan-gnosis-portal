0e29f1b7094ff3bc1aba0f92eecde68bb0f5e8f6
Daniel Riezebos <danielriezebos@outlook.com>
2024-08-14 15:35:31 +0200

    Added and implemented user-login system

--- migrations/Version20240805140247.php
+++ migrations/Version20240805140247.php
@@ -22,9 +22,9 @@ final class Version20240805140247 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE role (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
 
-        // Add "Administrator" and "User" roles
-        $this->addSQL('INSERT INTO role VALUES (1, "Administrator", "Enables Gnosis Projects and User management")');
-        $this->addSQL('INSERT INTO role VALUES (2, "User", "Enabled participation of Gnosis Projects")');
+        // Add "ROLE_ADMINISTRATOR" and "ROLE_USER" roles
+        $this->addSQL('INSERT INTO role VALUES (1, "ROLE_ADMINISTRATOR", "Enables Gnosis Projects and User management")');
+        $this->addSQL('INSERT INTO role VALUES (2, "ROLE_USER", "Enabled participation of Gnosis Projects")');
     }
 
     public function down(Schema $schema): void
