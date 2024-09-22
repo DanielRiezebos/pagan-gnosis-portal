e6e17653f7fea4f078ce5c9db5c5683b68e9921f
Daniel Riezebos <danielriezebos@outlook.com>
2024-09-22 16:18:17 +0200

    Added deletion of Gnosis Entries

--- src/Form/GnosisEntryType.php
+++ src/Form/GnosisEntryType.php
@@ -9,12 +9,13 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class GnosisEntryType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $builder->add('gnosis');
+        $builder->add('gnosis', TextareaType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
