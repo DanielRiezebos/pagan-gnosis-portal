ae5261ad202c6723ff7aadb3a275e74d7f70aa81
Daniel Riezebos <daniel@wiljeonline.nl>
2026-02-20 11:36:17 +0100

    WIP: Continued the work with the Tag system

---
+++ migrations/Version20260220102756.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260220102756 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('CREATE TABLE gnosis_project_tag (gnosis_project_id INT NOT NULL, tag_id INT NOT NULL)  DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('DROP TABLE gnosis_project_tag');
+    }
+}
